d2925c9f69670ee4856b1079c18bdf0853e97c20
pratheesh11 <46593026+pratheesh11@users.noreply.github.com>
2019-02-26 13:24:29 +0530

    Create MINIMUM.py

---
+++ MINIMUM.py
@@ -0,0 +1,16 @@
+#prathu
+#pratheesh
+a,b=map(str,input().split())
+u=0
+f=0
+for i in range(0,len(a)):
+   if(len(b)<=i):
+       f=1
+       break;
+   else:
+     if(a[i]==b[i]):
+        u+=1
+if(f==1):
+    print(len(a)-u)
+else:    
+    print(len(b)-u)
