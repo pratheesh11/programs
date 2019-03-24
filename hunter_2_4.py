61513254d729288dc763228d1d2576307c2e524b
pratheesh11 <46593026+pratheesh11@users.noreply.github.com>
2019-03-24 14:42:03 +0530

    Update hunter_2_4.py

--- hunter_2_4.py
+++ hunter_2_4.py
@@ -1,10 +1,11 @@
-#pratheesh
-from itertools import permutations
-
-in_str = input()
-
-out_list = permutations(in_str)
-out_list = list(dict.fromkeys(out_list))
-
-for perm in out_list:
-    print(''.join(perm))
+#juu
+#bebe
+n=int(input())
+n1=list(map(int,input().split()))
+s=[]
+for i in range(0,n-1):
+  if n1[i]>max(n1[i+1:]):
+    s.append(n1[i])
+s.append(n1[n-1])
+print(*s)
+print(max(n1))
