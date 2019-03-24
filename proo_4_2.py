c775d3411ababda21562b89a2c8118e1b41f8224
pratheesh11 <46593026+pratheesh11@users.noreply.github.com>
2019-03-24 18:36:20 +0530

    Create proo_4_2.py

---
+++ proo_4_2.py
@@ -0,0 +1,16 @@
+#pratheesh
+a,b=map(int,input().split())
+if a<=b:
+  d=a
+else:
+  d=b
+c=[]
+for i in range(0,d):
+  c.append(sorted(list(map(int,input().split()))))
+c=sorted(c)
+for i in range(0,len(c[0])):
+  for j in range(0,len(c)-1):
+    if c[j][i]>c[j+1][i]:
+      c[j][i],c[j+1][i]=c[j+1][i],c[j][i]
+for i in c:
+  print(*i)
