c73ce199b7b66671cb16c819698676369624c9c2
pratheesh11 <46593026+pratheesh11@users.noreply.github.com>
2019-03-24 14:38:28 +0530

    Create hunter_2_5.py

---
+++ hunter_2_5.py
@@ -0,0 +1,10 @@
+#pratheesh
+n=int(input())
+n1=list(map(int,input().split()))
+s=[]
+for i in range(0,n-1):
+  if n1[i]>max(n1[i+1:]):
+    s.append(n1[i])
+s.append(n1[n-1])
+print(*s)
+print(max(n1))
