739e0db04d06a8bef0191c521231e1fcd2ce6e17
pratheesh11 <46593026+pratheesh11@users.noreply.github.com>
2019-03-24 11:17:41 +0530

    Create porr_3_1.py

---
+++ porr_3_1.py
@@ -0,0 +1,12 @@
+#pratheesh
+n=int(input())
+n1=list(map(int,input().split()))
+a1,a2=[],[]
+for i in range(0,n-1):
+    a1=n1[:i+1]
+    a2=n1[i+1:]
+    if sum(a1)//len(a1)==sum(a2)//len(a2):
+      print('yes')
+      break
+else:
+  print('no')
