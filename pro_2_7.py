afb202cc687f608f55c18dbab4bf70727079ebc1
pratheesh11 <46593026+pratheesh11@users.noreply.github.com>
2019-03-24 10:37:34 +0530

    Create pro_2_7.py

---
+++ pro_2_7.py
@@ -0,0 +1,11 @@
+#pratheesh
+importsys, string, math
+n,k = input().split()
+n,k = int(n), int(k)
+L = [ int(x) for x in input().split()]
+for i in range(0,n-1) :
+    for j in range(i+1,n) :
+        if L[i] + L[j] == k :
+            print('yes')
+            sys.exit()
+print('no')
