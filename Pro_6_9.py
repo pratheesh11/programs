3bd51c936bbe1160fc51aebb85675d42d0befc56
pratheesh11 <46593026+pratheesh11@users.noreply.github.com>
2019-06-12 20:51:02 +0530

    Create Pro_6_9.py

---
+++ Pro_6_9.py
@@ -0,0 +1,10 @@
+#prathu
+a= input()
+b = input()
+c = a.split('|')
+if len(c[0]+b)==len(c[1]):
+  print(c[0]+b+'|'+c[1])
+elif len(c[0])==len(c[1]+b):
+  print(c[0]+'|'+c[1]+b)
+else:
+  print("Impossible")
