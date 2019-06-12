a995a2aea175dd784a8df4ad09039d9cb6a87e62
pratheesh11 <46593026+pratheesh11@users.noreply.github.com>
2019-06-12 21:07:31 +0530

    Create Pro_7_5.py

---
+++ Pro_7_5.py
@@ -0,0 +1,9 @@
+#prathu
+y1,y2 = map(int,input().split())
+m = list(map(int,input().split()))
+amount = int(input())
+total = (sum(m)-m[y2])//2
+if amount == total:
+  print("Bon Appetit")
+else:
+  print(amount-total)
